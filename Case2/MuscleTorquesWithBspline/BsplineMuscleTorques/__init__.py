e80e21dcc39ccfa2f5fa5bd02f56a1e6c5f7bc86
Noel Naughton <noel@ubuntu.localhost>
2022-01-17 10:29:08 -0600

    updating the MuscleTorquesWithBspline BC to be compatible with pyelastica>0.2.0

--- Case2/MuscleTorquesWithBspline/BsplineMuscleTorques/__init__.py
+++ Case2/MuscleTorquesWithBspline/BsplineMuscleTorques/__init__.py
@@ -1,13 +1,5 @@
 __all__ = ["MuscleTorquesWithVaryingBetaSplines"]
 
-from elastica import IMPORT_NUMBA
-
-if IMPORT_NUMBA:
-    from MuscleTorquesWithBspline.BsplineMuscleTorques.muscle_torques_with_bspline_numba import (
-        MuscleTorquesWithVaryingBetaSplines,
-    )
-
-else:
-    from MuscleTorquesWithBspline.BsplineMuscleTorques.muscle_torques_with_bspline_numpy import (
-        MuscleTorquesWithVaryingBetaSplines,
-    )
+from MuscleTorquesWithBspline.BsplineMuscleTorques.muscle_torques_with_bspline_numba import (
+    MuscleTorquesWithVaryingBetaSplines,
+)
